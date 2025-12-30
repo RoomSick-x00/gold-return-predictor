6ad5fe065046037bb375e4dc8b0761bcf548daf5
RoomSick-x00 <mail.priyanshu.79@gmail.com>
2025-12-30 23:55:54 +0530

    feat: add target profit price estimation

--- index.js
+++ index.js
@@ -90,8 +90,8 @@ function askInvestment(count, n, currentGoldPrice) {
           console.log("Investment Summary: ");
           console.log("Total Invested Amount:", result.totalInvested);
           console.log("Total Quantity (grams):", result.totalQuantity);
-          console.log("Portfolio break-even price per gram:",result.breakEvenPricePerGram);
-          console.log("Current Portfolio Value:", currentValue);
+          console.log("Portfolio break-even price per gram:",result.breakEvenPricePerGram.toFixed(2));
+          console.log("Current Portfolio Value:", currentValue.toFixed(2));
           console.log("--------------------------------");
           if (currentGoldPrice > result.breakEvenPricePerGram) {
             const profit =(currentGoldPrice - result.breakEvenPricePerGram)*result.totalQuantity;
@@ -104,7 +104,19 @@ function askInvestment(count, n, currentGoldPrice) {
           } else {
             console.log("Status: BREAK-EVEN");
           }
-          rl.close();
+          rl.question("Enter target profit amount: ", (targetProfit)=>{
+            const targetProfitAmount = Number(targetProfit);
+            if (isNaN(targetProfitAmount) || targetProfitAmount < 0) {
+              console.log("Invalid target profit amount");
+              rl.close();
+              return;
+            }
+            const targetPricePerGram = (result.totalInvested + targetProfitAmount) / result.totalQuantity;
+            console.log(`To earn ₹${targetProfitAmount},\nGold must reach ₹, ${targetPricePerGram.toFixed(2)} per gram.`);
+            const extraAboveBreakEven =targetPricePerGram - result.breakEvenPricePerGram;
+            console.log(`That is ₹${extraAboveBreakEven.toFixed(2)} above break-even price`);
+            rl.close();
+          });
         }
       });
     });
